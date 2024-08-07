9f5e11c55cd9063e84f530b9d306efd069378e4a
manjunath_G14 <manjunath@i-cronus.com>
2024-08-07 17:01:44 +0530

    first Commit

--- Tour and Travel portal with admin panel/booking core/booking-core-ultimate-booking-system/booking-core-ultimate-booking-system/booking-core.2.3.0/booking-core.2.3.0/modules/Media/Routes/admin.php
+++
@@ -1,5 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-Route::get('/','MediaController@index')->name('media.admin.index');
-
-Route::post('/edit_image','MediaController@editImage')->name('media.edit.image');
